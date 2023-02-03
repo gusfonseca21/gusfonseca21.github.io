ff3239708aea62db198978c971bc94e10ae990fe
Gustavo Fonseca <gustavo.fonseca@cleversystems.com.br>
2023-02-03 18:23:48 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://gusfonseca21.github.io/",
+  base: "/gusfonseca21.github.io/",
   plugins: [react()],
 });
