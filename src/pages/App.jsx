36e0b453e48f864d015235e2c19a1daadb8e5b24
Gustavo Fonseca <gustavo.fonseca@cleversystems.com.br>
2023-02-06 21:22:28 -0300

    Protótipo de tela de início do jogo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,27 +1,21 @@
-import { useState } from "react";
 import AnimatedBackground from "../components/AnimatedBackground";
 import "./App.css";
+import reactImg from "../assets/react.svg";
 
 function App() {
   return (
     <AnimatedBackground>
-      <div className='inputDiv'>
-        <label className='inputLabel'>Digite um texto</label>
-        <input
-          type='text'
-          className='inputBox'
-          onChange={(event) => setUser1Text(event.target.value)}
-        />
-        {/* <button>Poste o texto</button> */}
-      </div>
-      <div className='textsDiv'>
-        <div className='userDiv'>
-          <h1>Usuário 1</h1>
-          <h2 className='userText'>Teste</h2>
-        </div>
-        <div className='userDiv'>
-          <h1>Usuário 2</h1>
-          <h2 className='userText'>Aa</h2>
+      <div className='root'>
+        <div className='loginBox'>
+          <div className='profilePictureBox'>
+            <label className='profileLabel'>Escolha seu avatar</label>
+            <img className='profilePicture' src={reactImg} />
+          </div>
+          <div className='nameInputBox'>
+            <label className='inputLabel'>Digite seu nome</label>
+            <input type='text' className='nameInput' />
+          </div>
+          <button className='startButton'>Iniciar</button>
         </div>
       </div>
     </AnimatedBackground>
